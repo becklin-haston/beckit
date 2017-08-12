a052e23ed126226c07dda0d89f3dff5accf56de2
Becklin <nuclearb@gmail.com>
2017-08-12 13:25:33 -0700

    Taking in two parameters: a subreddit name and a number of links to display

--- beckit.py
+++ beckit.py
@@ -3,13 +3,28 @@ import os
 
 reddit = praw.Reddit(client_id="SsZW8NMLOD5qvg", client_secret="Pm4_MfJ_DMugRRDiEryG48CJbak", user_agent="Beckit: Beck's personal Reddit client")
 
+learnpython = "learnpython"
 
 
+subreddit_name = input("Which subreddit would you like to go to?")
+
+subreddit_limit = input("How many links do you want to display?")
+
+subreddit = reddit.subreddit(subreddit_name)
+
+subreddit_top_10_posts = subreddit.hot(limit=subreddit_limit)
+
+for submission in subreddit_top_10_posts:
+    print(submission.title)
+
+
+
+
+
+'''
 g = "go"
 
 i = str(input("Input:"))
-
-
 
 while i == "go":
 
@@ -20,7 +35,7 @@ while i == "go":
 
     if i == "go":
         os.system("clear")
-
+'''
 
 
 
